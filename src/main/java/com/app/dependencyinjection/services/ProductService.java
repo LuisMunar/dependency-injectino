a3a34e7fc42b77a514479f63ceea0556507935cd
LuisMunar <luis.emunarr@gmail.com>
2024-04-08 15:11:26 -0500

    Video 47 ready

---
+++ src/main/java/com/app/dependencyinjection/services/ProductService.java
@@ -0,0 +1,22 @@
+package com.app.dependencyinjection.services;
+
+import java.util.List;
+
+import com.app.dependencyinjection.models.Product;
+import com.app.dependencyinjection.repositories.ProductRepository;
+
+public class ProductService {
+  private ProductRepository productRepository;
+
+  public ProductService() {
+    this.productRepository = new ProductRepository();
+  }
+
+  public List<Product> getProducts() {
+    return productRepository.getProducts();
+  }
+
+  public Product getProductById(Number id) {
+    return productRepository.getProductById(id);
+  }
+}
